55a9f1530f4a3b1656ad5068115e3706d4967a7c
Kyungrae Kim <jeremiah.kim@hotmail.com>
2020-08-12 16:22:40 -0700

    Dockerfile updated 13

--- TinyPlants/TinyPlants/entrypoint.sh
+++ TinyPlants/TinyPlants/entrypoint.sh
@@ -3,8 +3,6 @@
 set -e
 run_cmd="ASPNETCORE_URLS=http://*:$PORT dotnet TinyPlants.dll"
 
-dotnet tool install --global dotnet-ef
-
 until dotnet-ef database update; do
 >&2 echo "StoreDbContext updated"
 sleep 1

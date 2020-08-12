cdfc57085da2e6f9cf64763bc410ed509576ca9b
Kyungrae Kim <jeremiah.kim@hotmail.com>
2020-08-12 10:44:42 -0700

    migration added and invenoty controller completed

---
+++ TinyPlants/TinyPlants/Controllers/InventoryController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TinyPlants.Models.Interfaces;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TinyPlants.Controllers
+{
+    public class InventoryController : Controller
+    {
+        private readonly IInventoryManager _inventories;
+
+        public InventoryController(IInventoryManager inventories)
+        {
+            _inventories = inventories;
+        }
+
+        // GET: /<controller>/
+        public async Task<IActionResult> IndexAsync()
+        {
+            return View(await _inventories.GetProducts());
+        }
+    }
+}
---
+++ TinyPlants/TinyPlants/Migrations/20200812174247_initial.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TinyPlants.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Sku = table.Column<string>(nullable: true),
+                    Name = table.Column<string>(nullable: true),
+                    Price = table.Column<decimal>(type: "money", nullable: false),
+                    Description = table.Column<string>(nullable: true),
+                    Image = table.Column<string>(nullable: true),
+                    IsFeatured = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.ID);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "ID", "Description", "Image", "IsFeatured", "Name", "Price", "Sku" },
+                values: new object[] { 1, "A combination of multiple small cactuses", "", true, "Atlantic", 12m, "CAC001" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products");
+        }
+    }
+}

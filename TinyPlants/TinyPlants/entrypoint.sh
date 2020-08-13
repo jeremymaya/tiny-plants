f9065f154fa38aac6d43492e843907add24231d7
Kyungrae Kim <jeremiah.kim@hotmail.com>
2020-08-12 17:56:55 -0700

    Dockerfile updated 17

--- TinyPlants/TinyPlants/entrypoint.sh
+++ TinyPlants/TinyPlants/entrypoint.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# set -e
-# run_cmd="ASPNETCORE_URLS=http://*:$PORT dotnet TinyPlants.dll"
+set -e
+run_cmd="ASPNETCORE_URLS=http://*:$PORT dotnet TinyPlants.dll"
 
 until dotnet-ef database update; do
 >&2 echo "StoreDbContext updated"
 sleep 1
 done
 
-# >&2 echo "SQL Server is up - executing command"
-# exec $run_cmd
+>&2 echo "SQL Server is up - executing command"
+exec $run_cmd
